1caef7cf24dcece83472fa93741888ae325e0a45
BentNai1 <eattopramen@hotmail.com>
2021-01-27 20:18:42 -0500

    Timer and Prompt Text

---
+++ Ten Second Defuse Game/Assets/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float timer = 0f;
+    Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        timerText.text = "Timer: " + Mathf.Clamp(12f - Mathf.Round(timer * 100f) / 100f, 0f, 10f);
+    }
+}
