dad5e1ebc19a3075367e5aac73b4bb0132b639b0
BentNai1 <eattopramen@hotmail.com>
2021-01-28 21:21:23 -0500

    Missed an animation, NOW they are all in

--- Ten Second Defuse Game/Assets/ClickMe.cs
+++ Ten Second Defuse Game/Assets/ClickMe.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ClickMe : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
+    public BoxCollider2D boxCollider2D;
     public Sprite cutWireSprite;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,11 @@ public class ClickMe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(BombSpriteChange.caseOpen == true && spriteRenderer.enabled == false)
+        {
+            spriteRenderer.enabled = true;
+            boxCollider2D.enabled = true;
+        }
     }
 
     private void OnMouseDown()
